f9d1e4ce6aafeda42bc84e28eb564f9a0798edd4
Francisco Cunha <francisco.cunha@deliverect.com>
2025-05-20 18:42:00 +0200

    Update mocked `io`

--- mock-utils.ts
+++ mock-utils.ts
@@ -1,5 +1,6 @@
 import { vi } from "vitest";
 import { MockedSocketContext } from "./mocked-socket-io-client";
+import { MockedSocketIo } from "./src/types";
 
 export const mockSocketIo = (
   io: unknown,
@@ -14,7 +15,6 @@ export const mockSocketIo = (
   // @ts-expect-error - we're mocking just the public API.
   vi.mocked(io).mockImplementation(() => {
     return {
-      // Socket properties
       get connected() {
         return context.client.getAttribute("connected");
       },
@@ -30,14 +30,28 @@ export const mockSocketIo = (
       get id() {
         return context.client.getAttribute("id");
       },
-
-      // Actions
-      on: context.client.mockOn,
-      once: context.client.mockOnce,
-      off: () => {}, // TODO
+      open: context.client.mockOpen,
+      close: context.client.mockClose,
+      connect: context.client.mockConnect,
+      disconnect: context.client.mockDisconnect,
+      compress: context.client.mockCompress,
+      timeout: context.client.mockTimeout,
+      send: context.client.mockSend,
       emit: context.client.mockEmit,
-      // TODO
-    };
+      emitWithAck: context.client.mockEmitWithAck,
+      listeners: context.client.mockListeners,
+      listenersAny: context.client.mockListenersAny,
+      listenersAnyOutgoing: context.client.mockListenersAnyOutgoing,
+      off: context.client.mockOff,
+      offAny: context.client.mockOffAny,
+      offAnyOutgoing: context.client.mockOffAnyOutgoing,
+      on: context.client.mockOn,
+      onAny: context.client.mockOnAny,
+      onAnyOutgoing: context.client.mockOnAnyOutgoing,
+      once: context.client.mockOnce,
+      prependAny: context.client.mockPrependAny,
+      prependAnyOutgoing: context.client.mockPrependAnyOutgoing,
+    } satisfies MockedSocketIo;
   });
 
   return {
