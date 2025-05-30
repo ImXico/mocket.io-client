e16d2f6e249b187b98a4d4547fb375347bf402f6
Francisco Cunha <francisco.cunha@deliverect.com>
2025-05-30 16:51:55 +0200

    Delete `custom.ts`

--- custom.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io-client";
-import { type MockedSocketContext } from "./mocked-socket-io-client";
-
-declare module "vitest" {
-  export interface TestContext {
-    mockedIo?: Socket;
-    mockedSocketContext?: MockedSocketContext;
-  }
-}
